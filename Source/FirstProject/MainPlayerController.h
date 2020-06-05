0689ab6933797965d4307aa417e5e4326251986f
Joe Horacki <prince_937@hotmail.com>
2020-06-05 08:23:42 -0600

    Lecture 52. HUD (Continued)

--- Source/FirstProject/MainPlayerController.h
+++ Source/FirstProject/MainPlayerController.h
@@ -14,4 +14,17 @@ class FIRSTPROJECT_API AMainPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+
+	// Reference UMG asset in the editor
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widgets")
+	TSubclassOf<class UUserWidget> HUDOverlayAsset;
+
+	//Variable to hold the widget after creating it
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widgets")
+	UUserWidget* HUDOverlay;
+
+protected:
+	virtual void BeginPlay() override;
+
 };
